eff4f4cd1961be0e247b047cad6abbcb8315eaed
Vinicius Dutra <candidodmv@gmail.com>
2019-11-14 17:54:19 -0300

    cookies handlers docs definitions

--- HybridWebView/Shared/HybridWebViewControl.cs
+++ HybridWebView/Shared/HybridWebViewControl.cs
@@ -26,10 +26,19 @@ namespace Plugin.HybridWebView.Shared
         /// </summary>
         public delegate Task ClearCookiesRequestDelegate();
 
+        /// <summary>
+        ///  Delegate to await getting all cookies. Returns string or string.Empty
+        /// </summary>
         public delegate Task<string> GetAllCookiesDelegate();
 
+        /// <summary>
+        ///  Delegate to await getting specified cookie from cookiename. Returns string or string.Empty
+        /// </summary>
         public delegate Task<string> GetCookieValueDelegate(string cookieName);
 
+        /// <summary>
+        ///  Delegate to await setting cookie by name and value. Returns cookievalue or string.Empty if something failed
+        /// </summary>
         public delegate Task SetCookieValueDelegate(string cookieName, string cookieValue, long? duration = null);
 
         /// <summary>
@@ -202,6 +211,7 @@ namespace Plugin.HybridWebView.Shared
         /// <summary>
         /// Getting all cookies from the current domain from shared storage
         /// </summary>
+        /// <returns>All cookies found for the current website. Returned on regular format "KEY=VALUE; KEY2=VALUE2". If no cookies where found, returns string.Empty</returns>
         public async Task<string> GetAllCookiesValueAsync()
         {
             if (OnGetAllCookiesRequested != null)
@@ -212,6 +222,8 @@ namespace Plugin.HybridWebView.Shared
         /// <summary>
         /// Getting a cookie value by cookiename.
         /// </summary>
+        /// <paramref name="cookieName">Cookie name to fetch</paramref>
+        /// <returns>A string with the cookievalue. is string.Empty if there is no cookie with that name</returns>
         public async Task<string> GetCookieValueAsync(string cookieName)
         {
             if (string.IsNullOrWhiteSpace(cookieName)) return string.Empty;
@@ -221,8 +233,11 @@ namespace Plugin.HybridWebView.Shared
         }
 
         /// <summary>
-        /// Setting a cookies value by cookiename.
+        /// Setting a cookie value by cookiename
         /// </summary>
+        /// <param name="cookieName">Cookie name to fetch</param>
+        /// <param name="duration">Expiration of cookie in seconds. If set to 0 or lower, the cookie is deleted. If not specified the cookie is set as sessioncookie and removed on app-close (Only works with iOS/macOS for now)</param>
+        /// <returns>A string with the cookievalue. is string.Empty if there is no cookie with that name</returns>
         public async Task SetCookieValueAsync(string cookieName, string cookieValue, long? duration = null)
         {
             if (string.IsNullOrWhiteSpace(cookieName)) return;
